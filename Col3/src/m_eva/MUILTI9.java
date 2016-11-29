675cab5d5218c54a0e431581f55a1521304ada50
usuario <usuario@usuario-HP-Compaq-6000-Pro-MT-PC>
2016-11-28 21:10:00 -0500

    Clase de Kevin LOjan

---
+++ Col3/src/m_eva/MUILTI9.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m_eva;
+
+/**
+ *
+ * @author usuario
+ */
+public class MUILTI9 {
+    public int n1;
+    public int n2;
+
+    public MUILTI9(int n1, int n2) {
+        this.n1 = n1;
+        this.n2 = n2;
+    }
+    
+    
+}
